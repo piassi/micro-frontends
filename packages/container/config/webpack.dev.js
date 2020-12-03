8516a9aadfe1b3db5105bb4e8888f9e1e03d30c4
Thales Nunes <Thales@8395-NB.local>
2020-12-03 20:34:51 -0300

    feat: add webpack production config

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,30 +1,26 @@
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const { dependencies } = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const { dependencies } = require("../package.json");
 
 const devConfig = {
-    mode: 'development',
-    devServer: {
-        port: 8080,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
     },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'container',
-            filename: 'remoteEntry.js',
-            remotes: {
-                marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-            },
-            shared: dependencies
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
-}
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      filename: "remoteEntry.js",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+      shared: dependencies,
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, devConfig);
