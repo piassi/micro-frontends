e6c75b116597703feea5f57e81fb47ee18ab2a73
Thales Nunes <Thales@8395-NB.local>
2020-12-04 20:16:14 -0300

    feat: update text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function App() {
     return (
         <div>
-            <h1>Container</h1>
+            <h1>Container!</h1>
             <MarketingApp />
         </div>
     )
