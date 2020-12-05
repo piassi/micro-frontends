7bcd11eda7c58d1c3a9557ea35cc25fb58640317
Thales Nunes <Thales@8395-NB.local>
2020-12-04 21:08:34 -0300

    feat: add Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default function App() {
     return (
+        <BrowserRouter>
         <div>
-            <h1>Container!</h1>
+            <Header />
             <MarketingApp />
         </div>
+        </BrowserRouter>
     )
 }
