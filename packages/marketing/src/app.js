1aa1207910dcf9ed65dbaa18e7ccaa6fbe9a8d7e
Thales Nunes <thales.piassi@ifood.com.br>
2021-01-07 15:40:23 -0300

    feat: add navigation

--- packages/marketing/src/app.js
+++ packages/marketing/src/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, Router } from 'react-router-dom'
 import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
@@ -8,16 +8,16 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'ma'
 });
 
-export default function App() {
+export default function App({ history }) {
     return (
         <div>
         <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
+            <Router history={history}>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing} />
                     <Route exact path="/" component={Landing} />
                 </Switch>
-            </BrowserRouter>
+            </Router>
         </StylesProvider>
         </div>
     )
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './app'
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if(onNavigate){
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     )
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const {pathname} = history.location;
+
+            if(pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 }
 
 if(process.env.NODE_ENV === 'development') {
     const el = document.getElementById('_marketing-root')
 
     if(el){
-        mount(el)
+        mount(el, { defaultHistory: createBrowserHistory() })
     }
 }
 
